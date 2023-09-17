b14d72546b95cd3b7e015464e3383a63b7a6bfd3
Amrul Izwan <amrulizwan7@gmail.com>
2023-09-17 22:18:47 +0800

    new crud

---
+++ backendflutter/create.php
@@ -0,0 +1,20 @@
+<?php
+
+    $connection = new mysqli("localhost", "root", "", "catatan");
+    $title      = $_POST['title']; 
+    $content    = $_POST['content'];
+    $date       = date('Y-m-d');
+    
+    $result = mysqli_query($connection, "insert into note_app set title='$title', content='$content', date='$date'");
+    
+    if($result){
+        echo json_encode([
+            'message' => 'Data input successfully'
+        ]);
+    }else{
+        echo json_encode([
+            'message' => 'Data Failed to input'
+        ]);
+    }
+
+    ?>
